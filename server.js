2167aa5fcdbaa59f0c091e6036c518abf70cf892
Fabio Gentile <37864192+Fabgen5@users.noreply.github.com>
2021-02-20 17:01:48 +0100

    Fix Data

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-var os = require("os");
-var hostname = os.hostname();
-let date_ob = new Date();
+
 
 // App
 const app = express();
 app.get('/', (req, res) => {
+  var os = require("os");
+  var hostname = os.hostname();
+  let date_ob = new Date();
 
   res.send(JSON.stringify('Ciao: ' + hostname +' Data: '+ date_ob));
 });
